ba71cef8828559afd21a41cc5c82675ed8a0acd5
Dana Ng <ngdana1@gmail.com>
2016-09-28 15:53:21 -0700

    setup capstone with routing and additional packages

---
+++ capstone/client/js/routes.js
@@ -0,0 +1,11 @@
+// set up the main template the the router will use to build pages
+Router.configure({
+    layoutTemplate: 'ApplicationLayout'
+});
+// specify the top level route, the page users see when they arrive at the site
+Router.route('/', function() {
+    console.log("Setting Up!");
+    this.render("navbar", { to: "nav" });
+    this.render("home", { to: "main" });
+    this.render("foot", { to: "footer" });
+});
