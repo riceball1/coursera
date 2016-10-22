4e56f7752bcc1a62f81d1822cf56fdb2d0a3f8ba
Dana Ng <ngdana1@gmail.com>
2016-10-22 11:49:40 -0700

    updated readme, added code for json iteration

--- capstone/readme.md
+++ capstone/readme.md
@@ -1,5 +1,10 @@
 
-#Resources
+#Description of the Capstone Course:
+
+In this course, you will develop more advanced web application programming skills. You will learn how to control data read and write access using methods, publish and subscribe. You will learn how to access your database and server shells using command line tools. You will use the SimpleSchema system to validate data and generate input forms automatically. You will see a complete collaborative code editing environment, TextCircle, being built from scratch. At the end of this course, you will be able to: - use Meteor methods to control data write access - use publish and subscribe to control data read access - install and use advanced Meteor packages - add user accounts to your applications - implement complex MongoDB filters - use the MongoDB and meteor server shells - define data validations schemas using SimpleSchema - generate data input forms automatically using SimpleSchema In this course, you will complete: 2 programming assignments taking ~4 hours each to complete 4 quizzes, each taking ~20 minutes to complete multiple practice quizzes, each taking ~5 minutes to complete Participation in or completion of this online course will not confer academic credit for University of London programmes
+
+
+
 
 ## packages used:
 - iron:router
@@ -9,9 +14,6 @@
 
 ## Info for features:
 - Full text search in Meteor https://www.okgrow.com/posts/guide-to-full-text-search-in-meteor
-
-
-
 
 TODO:
 
--- capstone/server/startup.js
+++ capstone/server/startup.js
@@ -9,26 +9,24 @@ Meteor.startup(() => {
         console.log("No data yet. Creating starter data.");
        
         // // JSON parse all files 
-        // var item1 = JSON.parse(Assets.getText('json/#.json'));
+        var items1 = JSON.parse(Assets.getText('json/household_items.json'));
         // var item2 = JSON.parse(Assets.getText('json/#.json'));
         // var item3 = JSON.parse(Assets.getText('json/#.json'));
         // // Items grouped together to be interated over and put into database
-        // var items_group = [item1, item2, item3];
+        var itemsGroup = [items1];
 
-        // // inserts each object as individual datasets
-        // var counter = 0;
-        // _.each(items_group, function(items_group){
-        //     items_group.forEach(function(item){
-        //         Facts.insert(item);
-        //         counter++;
-        //     });
-        // });
-        // console.log("The total data inserted " + counter);
-        // }
+        // forEach can only be used with an array of objects
+        var counter = 0;
+        _.each(itemsGroup, function(itemsGroup){
+            itemsGroup.forEach(function(item){
+                Items.insert(item);
+                counter++;
+            });
+        });
+        console.log("The total data inserted " + counter);
+        }
 
         if(Items.findOne()) {
             console.log("Server started up and data already loaded!");
         }
-
-    } // end of if have items
 });
